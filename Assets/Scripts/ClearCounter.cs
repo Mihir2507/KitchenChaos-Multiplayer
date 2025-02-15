2ba4da87e1adbad5cd3ee14d3c28ecc0f6cfb0b1
Mihir Chaudhary <83864106+Mihir2507@users.noreply.github.com>
2025-02-16 00:10:55 +0530

    unknown commit please check later.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem.iOS;
 
 public class ClearCounter : MonoBehaviour {
+
+    [SerializeField] KitchenObjectSO kitchenObjectSO;
+    [SerializeField] Transform counterTopPoint;
+
+    private KitchenObject kitchenObject;
+
     public void Interact () {
-        Debug.Log("Interact !");
+        if( kitchenObject == null){
+            Transform KitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            KitchenObjectTransform.localPosition = Vector3.zero;
+
+            kitchenObject = KitchenObjectTransform.GetComponent<KitchenObject>();
+            kitchenObject.SetClearCounter(this);
+        }
+        else {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+        
+        
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -81,7 +81,6 @@ public class Player : MonoBehaviour {
         else {
             SetSelectedCounter(null);
         }
-        Debug.Log(selectedCounter);
         
     }
 
